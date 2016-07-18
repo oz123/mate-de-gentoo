ffbfbf6d02f738290d77b91416cb01db9a1d3a41
root <oz.tiram@gmail.com>
2016-07-18 13:06:27 +0200

    fix build script

--- docs/install_all.sh
+++ docs/install_all.sh
@@ -2,21 +2,23 @@
 
 VERSION=1.14.0
 
-USE="gtk3" emerge -ank =mate-base/mate-common-1.14.0 =mate-base/mate-desktop-1.14.0 \
+USE="gtk3" emerge -ank =mate-base/mate-common-1.14.0 \
+=mate-base/mate-desktop-1.14.1 \
 =mate-base/libmatekbd-${VERSION}* \
 =dev-libs/libmateweather-${VERSION}* \
 =x11-themes/mate-icon-theme-${VERSION}* \
-=mate-base/caja-1.14.0 =x11-wm/marco-1.14.0 \
+=mate-base/caja-1.14.1 \
+=x11-wm/marco-1.14.0 \
 =media-libs/libmatemixer-${VERSION} \
 =mate-base/mate-settings-daemon-${VERSION} \
 =mate-base/mate-session-manager-${VERSION} \
 =mate-base/mate-menus-${VERSION} \
-=mate-base/mate-panel-${VERSION} \
+=mate-base/mate-panel-1.14.1 \
 =mate-base/mate-control-center-$VERSION \
 =mate-extra/mate-polkit-$VERSION \
 =mate-base/mate-applets-${VERSION}* \
 =mate-extra/mate-media-$VERSION \
-=mate-extra/mate-netbook-$VERSION \
+=mate-extra/mate-netbook-1.14.0 \
 =mate-extra/caja-extensions-$VERSION \
 =mate-extra/caja-dropbox-$VERSION \
 =x11-themes/mate-backgrounds-$VERSION \
@@ -29,9 +31,10 @@ USE="gtk3" emerge -ank =mate-base/mate-common-1.14.0 =mate-base/mate-desktop-1.1
 =dev-python/python-caja-$VERSION \
 =mate-extra/mate-power-manager-1.14.0 \
 =x11-terms/mate-terminal-${VERSION} \
-=app-arch/engrampa-$VERSION \
+=app-arch/engrampa-1.14.1 \
 =media-gfx/eom-${VERSION}* \
-=app-text/atril-1.14.0 \
-=mate-extra/mate-screensaver-${VERSION} \
+=app-text/atril-1.14.1 \
+=mate-extra/mate-screensaver-1.14.1 \
 =x11-misc/mozo-${VERSION} \
-=app-editors/pluma-1.14.0
+=app-editors/pluma-1.14.0 \
+=x11-themes/mate-themes-1.14.2
