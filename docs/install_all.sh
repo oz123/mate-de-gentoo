3b01719e67ab7dec785508a5f1cfa5c620c25409
root <oz.tiram@gmail.com>
2016-09-22 10:44:34 +0200

    Updat building the latest version of 1.14.x

--- docs/install_all.sh
+++ docs/install_all.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-VERSION=1.14.0
+VERSION=1.14.1
 
-USE="gtk3" emerge -ank =mate-base/mate-common-1.14.0 \
+USE="gtk3 xinerama" emerge -aUN =mate-base/mate-common-1.14.0 \
 =mate-base/mate-desktop-1.14.1 \
 =mate-base/libmatekbd-${VERSION}* \
-=dev-libs/libmateweather-${VERSION}* \
-=x11-themes/mate-icon-theme-${VERSION}* \
+=dev-libs/libmateweather-1.14.3 \
+=x11-themes/mate-icon-theme-1.14.0 \
 =mate-base/caja-1.14.1 \
 =x11-wm/marco-1.14.0 \
-=media-libs/libmatemixer-${VERSION} \
+=media-libs/libmatemixer-1.14.0 \
 =mate-base/mate-settings-daemon-${VERSION} \
 =mate-base/mate-session-manager-${VERSION} \
-=mate-base/mate-menus-${VERSION} \
+=mate-base/mate-menus-1.14.0 \
 =mate-base/mate-panel-1.14.1 \
 =mate-base/mate-control-center-$VERSION \
-=mate-extra/mate-polkit-$VERSION \
+=mate-extra/mate-polkit-1.14.0 \
 =mate-base/mate-applets-${VERSION}* \
 =mate-extra/mate-media-$VERSION \
 =mate-extra/mate-netbook-1.14.0 \
 =mate-extra/caja-extensions-$VERSION \
-=mate-extra/caja-dropbox-$VERSION \
-=x11-themes/mate-backgrounds-$VERSION \
+=mate-extra/caja-dropbox-1.14.0-r1 \
+=x11-themes/mate-backgrounds-1.14.0 \
 =x11-misc/mate-notification-daemon-$VERSION \
-=mate-extra/mate-sensors-applet-$VERSION \
+=mate-extra/mate-sensors-applet-1.14.0 \
 =mate-extra/mate-system-monitor-$VERSION \
-=mate-extra/mate-user-guide-$VERSION \
-=mate-extra/mate-user-share-$VERSION \
+=mate-extra/mate-user-guide-1.14.0 \
+#=mate-extra/mate-user-share-1.14.0 \
 =mate-extra/mate-utils-1.14.0 \
-=dev-python/python-caja-$VERSION \
+=dev-python/python-caja-1.14.0 \
 =mate-extra/mate-power-manager-1.14.0 \
 =x11-terms/mate-terminal-${VERSION} \
 =app-arch/engrampa-1.14.1 \
