oz123/mate-de-gentoo

commit eea7c910b28bd1e73f732d3597f6650b21fef67f

eclass: support EAPI8

eclass/mate-desktop.org.eclass
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 # @ECLASS: mate-desktop.org.eclass
@@ -6,15 +6,15 @@
 # mate@gentoo.org
 # @AUTHOR:
 # Authors: NP-Hardass <NP-Hardass@gentoo.org> based upon the gnome.org eclass.
-# @SUPPORTED_EAPIS: 6 7
+# @SUPPORTED_EAPIS: 7 8
 # @BLURB: Helper eclass for mate-desktop.org hosted archives
 # @DESCRIPTION:
 # Provide a default SRC_URI and EGIT_REPO_URI for MATE packages as well as
 # exporting some useful values like the MATE_BRANCH
 
-# EAPIs < 6 are banned.
+# EAPIs < 7 are banned.
 case "${EAPI:-0}" in
-	6|7) ;;
+	7|8) ;;
 	*) die "EAPI=${EAPI:-0} is not supported" ;;
 esac
 
@@ -22,7 +22,7 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 fi
 
-[[ ${EAPI:-0} -eq 6 ]] && inherit eapi7-ver
+[[ ${EAPI:-0} -eq 7 ]] && inherit eapi7-ver
 
 # @ECLASS_VARIABLE: MATE_TARBALL_SUFFIX
 # @INTERNAL

eclass/mate.eclass
@@ -7,7 +7,7 @@
 # @AUTHOR:
 # Authors: NP-Hardass <NP-Hardass@gentoo.org> based upon the gnome2
 # and autotools-utils eclasses
-# @SUPPORTED_EAPIS: 6 7
+# @SUPPORTED_EAPIS: 7 8
 # @BLURB: Provides phases for MATE based packages.
 # @DESCRIPTION:
 # Exports portage base functions used by ebuilds written for packages using the
@@ -16,7 +16,7 @@
 
 # Check EAPI only
 case "${EAPI:-0}" in
-	6|7) ;;
+	7|8) ;;
 	*) die "EAPI=${EAPI:-0} is not supported" ;;
 esac
 
@@ -39,7 +39,7 @@ GNOME2_LA_PUNT="${MATE_LA_PUNT}"
 inherit gnome2 autotools mate-desktop.org
 
 case "${EAPI:-0}" in
-	6|7) EXPORT_FUNCTIONS src_prepare src_configure src_install pkg_preinst pkg_postinst pkg_postrm ;;
+	7|8) EXPORT_FUNCTIONS src_prepare src_configure src_install pkg_preinst pkg_postinst pkg_postrm ;;
 	*) die "EAPI=${EAPI:-0} is not supported" ;;
 esac
 

commit bf3a63f4cbe7400c637dbc8b430b362807309f04

eclass: removes last usages of eapi7-ver
Fix bug introduced in commit eea7c910b28bd1e73f732d3597f6650b21fef67f.

Signed-off-by: Thibaud CANALE <thican@thican.net>

eclass/mate-desktop.org.eclass
@@ -22,8 +22,6 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 fi
 
-[[ ${EAPI:-0} -eq 7 ]] && inherit eapi7-ver
-
 # @ECLASS_VARIABLE: MATE_TARBALL_SUFFIX
 # @INTERNAL
 # @DESCRIPTION:

mate-base/mate/mate-1.26.0.ebuild
@@ -1,85 +1,0 @@
-# Copyright 1999-2021 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=6
-
-if [[ ${PV} == 9999 ]]; then
-	MATE_BRANCH=9999
-	MATE_THEMES_V=9999
-else
-	inherit eapi7-ver
-	MATE_BRANCH="$(ver_cut 1-2)"
-	MATE_THEMES_V=3
-	KEYWORDS="~amd64"
-fi
-
-SRC_URI=""
-DESCRIPTION="Meta ebuild for MATE, a traditional desktop environment"
-HOMEPAGE="https://mate-desktop.org"
-
-LICENSE="metapackage"
-
-SLOT="0"
-IUSE="+base -bluetooth help +notification +themes +extras"
-
-S="${WORKDIR}"
-
-RDEPEND="
-	=mate-base/mate-desktop-${MATE_BRANCH}*
-	=mate-base/mate-menus-${MATE_BRANCH}*
-	=mate-base/mate-panel-${MATE_BRANCH}*
-	=mate-base/mate-session-manager-${MATE_BRANCH}*
-	=mate-base/mate-settings-daemon-${MATE_BRANCH}*
-	=x11-wm/marco-${MATE_BRANCH}*
-	base? (
-		=mate-base/caja-${MATE_BRANCH}*
-		=mate-base/mate-applets-meta-${MATE_BRANCH}*
-		=mate-base/mate-control-center-${MATE_BRANCH}*
-		=mate-extra/mate-media-${MATE_BRANCH}*
-		=x11-misc/mozo-${MATE_BRANCH}*
-		=x11-terms/mate-terminal-${MATE_BRANCH}*
-	)
-	bluetooth? ( net-wireless/blueman )
-	themes? (
-		=x11-themes/mate-backgrounds-${MATE_BRANCH}*
-		=x11-themes/mate-icon-theme-${MATE_BRANCH}*
-		>=x11-themes/mate-themes-meta-${MATE_THEMES_V}
-	)
-	extras? (
-		=app-arch/engrampa-${MATE_BRANCH}*
-		=app-editors/pluma-${MATE_BRANCH}*
-		=app-text/atril-${MATE_BRANCH}*
-		=mate-extra/caja-extensions-${MATE_BRANCH}*
-		=mate-extra/mate-calc-${MATE_BRANCH}*
-		=mate-extra/mate-netbook-${MATE_BRANCH}*
-		=mate-extra/mate-power-manager-${MATE_BRANCH}*
-		=mate-extra/mate-screensaver-${MATE_BRANCH}*
-		=mate-extra/mate-system-monitor-${MATE_BRANCH}*
-		=mate-extra/mate-utils-${MATE_BRANCH}*
-		=media-gfx/eom-${MATE_BRANCH}*
-	)
-	help? (
-		gnome-extra/yelp
-		=mate-extra/mate-user-guide-${MATE_BRANCH}*
-	)
-"
-
-PDEPEND="
-	notification? ( =x11-misc/mate-notification-daemon-${MATE_BRANCH}* )
-	virtual/notification-daemon:0"
-
-pkg_postinst() {
-	elog "For installation, usage and troubleshooting details regarding MATE;"
-	elog "read more about it at Gentoo Wiki: https://wiki.gentoo.org/wiki/MATE"
-	elog ""
-	if ! has_version x11-misc/mate-notification-daemon; then
-		elog "If you experience any issues with notifications, please try using"
-		elog "x11-misc/mate-notification-daemon instead your currently installed daemon"
-		elog ""
-	fi
-	elog "Some packages that are not included in this meta-package but may be of interest:"
-	elog "		mate-extra/caja-dropbox"
-	elog "		mate-extra/mate-user-share"
-	elog "		mate-extra/caja-admin"
-	elog "		mate-extra/caja-hide"
-}